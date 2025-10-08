19d0c40a479df38b4bb8f118bfd9653d3801dfe9
YonteMoon <monteyoon1998@gmail.com>
2025-10-08 12:10:25 -0700

    remove comments.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite"
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-
-// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import tanstackRouter from "@tanstack/router-plugin/vite"
 import { resolve } from "node:path"
 import netlify from "@netlify/vite-plugin-tanstack-start"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     netlify(),
