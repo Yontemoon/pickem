1a766ce2e311fcc932507d4d8f548737fd4b9fb0
YonteMoon <monteyoon1998@gmail.com>
2025-10-08 15:56:59 -0700

    Added more to scraper

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,11 @@ import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import tanstackRouter from "@tanstack/router-plugin/vite"
 import { resolve } from "node:path"
-import netlify from "@netlify/vite-plugin-tanstack-start"
+// import netlify from "@netlify/vite-plugin-tanstack-start"
 
 export default defineConfig({
   plugins: [
-    netlify(),
+    // netlify(),
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
--- scraper/src/main.js
+++ scraper/src/main.js
@@ -1,10 +1,12 @@
-import puppeteer from "puppeteer"
+import puppeteer, { Browser } from "puppeteer"
 import { TAPOLOGY_URL } from "./lib/constants.js"
 import { delay } from "./lib/utils.js"
 
 async function main() {
   try {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      headless: false,
+    })
     const page = await browser.newPage()
     await page.goto(`${TAPOLOGY_URL}/fightcenter`, {
       waitUntil: "networkidle2",
@@ -25,17 +27,65 @@ async function main() {
       timeout: 15000,
     })
 
-    const boutData = await page.$$eval(
+    const eventsData = await page.$$eval(
       '[class="div flex flex-col border-b border-solid border-neutral-700"]',
-      (elements) => elements.map((el) => el.textContent.trim())
+      (elements) =>
+        elements.map((el) => {
+          const aTag = el.querySelector("a")
+          const title = aTag.textContent
+          return {
+            title: title,
+            href: aTag.href,
+          }
+        })
     )
+    console.group(`Found ${eventsData.length} bouts`)
+    console.log("Group Info", eventsData)
+    console.groupEnd()
 
-    console.log(`Found ${boutData.length} bouts`)
+    await Promise.all(eventsData.map((event) => getEventData(event, browser)))
 
     await browser.close()
   } catch (error) {
     console.error(error)
   }
+}
+
+/**
+ *
+ * @param {{
+ * title: string,
+ * href: string}} eventData
+ * @param {Browser} browser
+ */
+const getEventData = async (eventData, browser) => {
+  const eventPage = await browser.newPage()
+  await eventPage.goto(eventData.href, {
+    waitUntil: "networkidle2",
+    timeout: 60000,
+  })
+
+  const date = await eventPage.$eval(
+    '[class="div flex items-center justify-between text-xs uppercase font-bold text-tap_7f leading-none"]',
+    (element) => {
+      const spans = Array.from(element.querySelectorAll("span"))
+      return spans.map((span) => span.textContent.trim())[1]
+    }
+  )
+
+  const events = await eventPage.$$eval(
+    '[data-event-view-toggle-target="list"]',
+    (element) => {
+      return element.length
+    }
+  )
+
+  await delay(1000)
+  await eventPage.close()
+  console.groupCollapsed(eventData.title)
+  console.log("Date of Event:", date)
+  console.log("Number of Schedule bouts:", events)
+  console.groupEnd()
 }
 
 main()
