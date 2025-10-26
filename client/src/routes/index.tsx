27392089ee0511432fe27213c9538d8f1bab900a
YonteMoon <monteyoon1998@gmail.com>
2025-10-26 11:25:10 -0700

    added tpye to response

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router"
-import logo from "../logo.svg"
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -8,42 +7,29 @@ export const Route = createFileRoute("/")({
       `${import.meta.env.VITE_API_URL}/events/upcoming`
     )
     const data = await response.json()
-    console.log(data)
-    return data
+
+    return data as {
+      data:
+        | {
+            id: number
+            created_at: string
+            event_title: string
+            date: string
+          }[]
+        | null
+      error: string | null
+    }
   },
 })
 
 function App() {
-  const data = Route.useLoaderData()
+  const { data } = Route.useLoaderData()
   return (
     <div className="text-center">
-      {JSON.stringify(data)}
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+      {data &&
+        data.map((d) => {
+          return <div>{d.event_title}</div>
+        })}
     </div>
   )
 }
