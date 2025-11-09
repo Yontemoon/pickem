ebe6f39c6157f1c1e9af154555f3e22262e86a04
YonteMoon <monteyoon1998@gmail.com>
2025-11-09 15:23:43 -0800

    Removed noemit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
-import { getSupabase, supabaseMiddleware } from "../supabase/supabase.ts"
+import { getSupabase, supabaseMiddleware } from "../supabase/supabase.js"
 import { cors } from "hono/cors"
 
 const app = new Hono()
