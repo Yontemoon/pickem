e9b5c1f01b84e651b3298cfda224bfa6eb4fa266
YonteMoon <monteyoon1998@gmail.com>
2025-11-26 10:31:29 -0800

    added Header capitalized.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,18 +6,16 @@ const Header = () => {
   const { isAuthenticated } = useAuth()
 
   return (
-    <>
-      <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg justify-between">
-        <div className="flex items-center">
-          <h1 className="ml-4 text-xl font-semibold">
-            <Link to={isAuthenticated ? "/app" : "/"}>
-              <div>Pick'Em</div>
-            </Link>
-          </h1>
-        </div>
-        <ThemeToggle />
-      </header>
-    </>
+    <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg justify-between">
+      <div className="flex items-center">
+        <h1 className="ml-4 text-xl font-semibold">
+          <Link to={isAuthenticated ? "/app" : "/"}>
+            <div>Pick'Em</div>
+          </Link>
+        </h1>
+      </div>
+      <ThemeToggle />
+    </header>
   )
 }
 
